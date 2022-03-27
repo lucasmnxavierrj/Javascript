faf7fafad10881b501fd2bff99b45fedaa286c02
lucasmnxavier <lucasmatiasnicacioxavier@gmail.com>
2022-03-27 11:00:16 -0300

    adding project 7

---
+++ vanilla/7_Q&A/script.js
@@ -0,0 +1,19 @@
+let card = document.querySelectorAll('.card')
+let txtCard = document.querySelector('.texto-card')
+
+card.forEach(card =>{
+    //console.log(botao.parentElement.parentElement.childNodes[3].classList.contains('hide')) 
+    let botao = card.querySelector('.expandir-card')
+    let conteudoCard = card.querySelector('.texto-card')
+
+    botao.addEventListener('click',()=>{
+        if(conteudoCard.classList.contains('hide')){
+            conteudoCard.classList.remove('hide')
+            conteudoCard.style.animation = 'fadeIn 1s 1'
+            conteudoCard.style.opacity = '1'
+        }else{
+            conteudoCard.classList.add('hide')
+        }
+    })
+    console.log(botao, conteudoCard)
+})
